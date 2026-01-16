79d7d99376ad59d30622af03743e4421c204f33d
离线下载 <66726043+LiXianXiaZai@users.noreply.github.com>
2026-01-16 22:03:43 +0800

    Update Download.js

--- SenPlayer/Download.js
+++ SenPlayer/Download.js
@@ -1,3 +1,19 @@
-let obj = JSON.parse($response.body);
-obj.CanDownload = true
-$done({status: $response.status, headers: $response.headers, body: JSON.stringify(obj) });
+let url = $request.url;
+
+if(/\/emby\/users/i.test(url)) {
+    let obj = JSON.parse($response.body);
+    obj.CanDownload = true;
+    $done({body: JSON.stringify(obj)});
+} else if(/\/emby\/Shows/i.test(url)){
+	let obj = JSON.parse($response.body);
+	obj["Items"].forEach((item) => {
+		item.CanDownload = true
+	})
+	$done({body: JSON.stringify(obj)});
+} else if(/(PlaybackInfo|Videos)/i.test(url)) {
+    let headers = $request.headers;
+    headers['User-Agent'] = headers['User-Agent'].replace('_Download', '');
+    $done({headers: headers});
+} else {
+    $done({});
+}
